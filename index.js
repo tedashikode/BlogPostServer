1bfab1d108e0cb3048d7577718d06591e13c49e8
koder_ <57563873+tedashikode@users.noreply.github.com>
2021-10-29 17:47:24 +0000

    made some minor edits

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-    //Schemas
-    //typeDefs
-    //Resolvers
-    //Mutation
-
-    //Schemas
     const blogs = [
     
         {
@@ -50,13 +44,8 @@ const { ApolloServer, gql } = require('apollo-server');
     ];
     
     
-    
-    
-    //the schema for the server
     const schemas = gql`
-        #what does type even mean in graphql???
         type Blog {
-            #the fields every blog should have
             blogID: ID
             title: String!
             author: String!
@@ -66,84 +55,21 @@ const { ApolloServer, gql } = require('apollo-server');
             likes: Int
             dislikes: Int
         }
-
-        #I guess we should add types for all our mandatory fields
-
-        #type Author{}
-        #type Comment{}
-        
-        
+               
         type Query {
             allTheBlogs: [Blog]
             blogByID(blogID: ID): Blog
             DateOfBlog: String!
         }
-        
-        #type Mutation
-      #  type Mutation   createBlog(title: String!, author: String!, DateOFBlog: String!): Blog      }
-        
-        #type Subscription
-    `;
-    
-    //Resolvers
+    `
+
     const blogResolvers = {
         Query: {
             allTheBlogs: () => blogs,
             blogByID: (parent, args) => blogs.find(blogs1 => blogs.title === args.title)
         },
-    
-        // Mutation: {
-        //     createBlogs: ( parents, args) => {
-                
-        //         const { title, author, DateOfBlog } = args;
-    
-        //         const blog = { title, author, DateOfBlog };
-    
-        //         blogs.push(blog);
-    
-        //         return(blog);
-    
-        //     },
+    }
 
-            // deleteBlogs: ( parents, args) => {
-                
-            //     const { title, author, DateOfBlog } = args;
-    
-            //     const blog = { title, author, DateOfBlog };
-    
-            //     blogs.push(blog);
-    
-            //     return(blog);
-    
-            // },
-
-            // updateBlogs: ( parents, args) => {
-                
-            //     const { title, author, DateOfBlog } = args;
-    
-            //     const blog = { title, author, DateOfBlog };
-    
-            //     blogs.push(blog);
-    
-            //     return(blog);
-    
-            // },
-
-            // Blogs: ( parents, args) => {
-                
-            //     const { title, author, DateOfBlog } = args;
-    
-            //     const blog = { title, author, DateOfBlog };
-    
-            //     blogs.push(blog);
-    
-            //     return(blog);
-    
-            // },
-        }
-        // Mutation: {}
-    
-    
     const serve = new ApolloServer({ typeDefs: schemas, resolvers: blogResolvers});
     
     serve.listen(9000)
